6460368f5c23208e0f50f2c3ced54c196f79b48a
eattang <eattang@gmail.com>
2014-04-23 22:26:00 -0500

    commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,61 @@
 ## functions do
 
 ## Write a short comment describing this function
+##inv_ is used to store the inverse matrix
+##data is the refrence matrix
+#assume that the matrix x is always square and hence, invertible
 
 makeCacheMatrix <- function(x = matrix()) {
+	inv_<-NULL
+
+
+	set<- function(y){
+		data<<-y	
+		inv_<<-NULL	
+	
+	}
+	
+	get<- function(){
+		data
+	}
+
+
+	set_inv<-function(inv2_)
+	{
+		inv_<<-inv2_
+	}
+
+	get_inv<-function(){
+		inv_
+	}
+## returns a list as the output	
+
+	list(set=set,get=get,set_inv=set_inv,get_inv=get_inv)
 
 }
 
 
 ## Write a short comment describing this function
+##invertible matrices must have a non zero determinant
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$get_inv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+	#get the inverse of the matrix
+	if (det(data) != 0){
+
+		m<-solve(data)
+        	x$set_inv(m)
+        	return(m)
+	}else{
+
+		stop('Matrix cannot be inverted')
+	}
+	
+
 }
